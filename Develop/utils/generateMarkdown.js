90b71d483e2575c4986947b1f0e7cfe27bee9e7f
Cinderbeast <105569378+Cinderbeast@users.noreply.github.com>
2022-08-06 13:41:55 -0600

    Initial code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,68 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const generatelicense = (type) => {
+  let color;
+  if (type === "ISC") color = "goldenrod";
+  if (type === "Apache") color = "blue";
+  if (type === "MPL") color = "green";
+  if (type === "CC") color = "purple";
+  if (type === "NONE") color = "red";
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  return (
+    `
+## License
+<img src="https://img.shields.io/badge/license-${type}-${color}" alt="badge-${type}" height="44" />
+  `
+  );
+};
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contribution,
+  test,
+  github,
+  email,
+}) => {
 
-`;
-}
+  console.log("GENERATING PAGE...");
+  const template = (
+    `
+# ${title}
+## Description
+${description}
+## Table of Contents
+  1. [installation](#installation)
+  2. [usage](#usage)
+  3. [contribution](#contribution)
+  4. [test](#test)
+  5. [github](#github)
+  6. [email](#email)
+  7. [license](#license)
+## Installation
+${installation}
+## Usage
+${usage}
+## Contribution
+${contribution}
+## Test 
+${test}
+## GitHub: 
+${github}
+## Email: 
+${email}
+${generatelicense(license)}
+    `
+  );
 
-module.exports = generateMarkdown;
+  fs.writeFileSync('./output/README.md', template);
+  console.log('TEMPLATE GENERATED!');
+  process.exit();
+};
+
+module.exports = {
+  generateMarkdown
+};
